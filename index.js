3aa72e477426e44e242ec1875c8e81b41973b47d
wanderer <mjbecze@gmail.com>
2016-02-06 16:05:46 -0500

    intial impelentation

--- index.js
+++ index.js
@@ -1,32 +1,111 @@
 'use strict'
-// generally immutble
 
+class EdgeMap extends Map {
+  set (name, vertex) {
+    if (vertex.constructor.name === 'DAG') {
+      return super(name, vertex)
+    }
+    throw new Error('invalid vertex')
+  }
+}
+
+// A very generic Directed acyclic graph implementation
 module.exports = class DAG {
-  constructor () {
-    this._links = {}
+  /**
+   * Create a new vertex
+   * @param {DAG} vertex a vertex to copy or an intial value
+   */
+  constructor (vertex) {
+    this._edges = new EdgeMap()
     this._value = null
+
+    if (vertex) {
+      // copy constructor
+      if (vertex.constructor.name === 'DAG') {
+        Object.assign(this, vertex)
+      } else {
+        this.value = vertex
+      }
+    }
   }
 
-  set value (value) { this._value = value }
-  get value () { return this._value }
-
-  get links () {
-    return this._links
+  get edges () {
+    return this._edges
   }
 
-  setLink (name, node) { }
+  get value () {
+    return this._value
+  }
 
-  set (path, node) {}
-  get (path) {}
-  del (path) {}
+  set value (val) {
+    this._value = val
+    return val
+  }
 
-  getRoot (path) {}
-  setRoot (path, ref) {}
-  delRoot (path) {}
+  set (path, vertex) {
+    path = !Array.isArray(path) ? [path] : path
+    let name = path.pop()
+
+    if (!path.length) {
+      this.edges.set(name, vertex)
+      return
+    }
+
+    let nextVertex = this.edges.get(name)
+    if (!nextVertex) {
+      nextVertex = new DAG()
+      this.edges.set(path, nextVertex)
+    }
+
+    nextVertex.set(path, vertex)
+  }
+
+  get (path) {
+    path = !Array.isArray(path) ? [path] : path
+    let name = path.pop()
+
+    if (!path.length) {
+      return this.edges.get(name)
+    }
+
+    let nextVertex = this.edges.get(name)
+    if (!nextVertex) {
+      return
+    }
+
+    nextVertex.get(path)
+  }
+
+  delete (path) {
+    path = !Array.isArray(path) ? [path] : path
+    let name = path.pop()
+
+    if (!path.length) {
+      return this.edges.delete(name)
+    }
+
+    let nextVertex = this.edges.get(name)
+    if (!nextVertex) {
+      return false
+    }
+
+    nextVertex.get(path)
+  }
 
   iterDepth () {}
   iterBreath () {}
   iterPath (path) {}
-
-  _linkHandler () {}
 }
+
+/**
+ * immutable notes
+  getRoot (path) {}
+  setRoot (path, ref) {}
+  delRoot (path) {}
+
+  batch (ops) {
+    // NO READS ALLOWED
+    // build ops tree
+    // at every branch of the operation tree we can parrilize
+  }
+**/
