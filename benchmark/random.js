9e8a2cba96e12f168981763b4a626bf2e51e2bad
wanderer <mjbecze@gmail.com>
2016-02-07 20:54:27 -0500

    added benchmarks

---
+++ benchmark/random.js
@@ -0,0 +1,45 @@
+// https://github.com/ethereum/wiki/wiki/Benchmarks
+'use strict'
+const Graph = require('../')
+
+const MAX_PATH_LENTH = 1
+const ROUNDS = 1000000
+
+let paths = generateData()
+
+let testName = 'random'
+console.time(testName)
+randomPathInsertions(paths)
+console.timeEnd(testName)
+
+testName = 'random deletes'
+console.time(testName)
+randomPathInsertionsAndDeletions(paths)
+console.timeEnd(testName)
+
+function generateData () {
+  let paths = []
+  for (let i = 0; i < ROUNDS; i++) {
+    let path = new Array(Math.random() * MAX_PATH_LENTH | 0).fill(Symbol())
+    paths.push(path)
+  }
+  return paths
+}
+
+function randomPathInsertions (data) {
+  let graph = new Graph()
+  data.forEach((path) => {
+    graph.set(path, Symbol())
+  })
+}
+
+function randomPathInsertionsAndDeletions (data) {
+  let graph = new Graph()
+  data.forEach((path) => {
+    graph.set(path, Symbol())
+  })
+
+  data.forEach((path) => {
+    graph.delete(path)
+  })
+}
